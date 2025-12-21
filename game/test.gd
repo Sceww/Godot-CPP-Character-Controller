d0df90693affc432638460f04422843b7157ef39
Dylan <dylanjmsguzman@gmail.com>
2025-12-21 17:41:45 -0600

    testing...

---
+++ game/test.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var s = ExampleClass.new() # OoOOOOoooOOOHHHH
+	s.print_type(Shape3D)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
